f4d72e863a0e1a16a9ff56fb7889adbba312d2c7
tanishiking <rikiriki1238@gmail.com>
2015-06-12 14:18:36 +0900

    bubble sort dayogit add bubble.cgit add bubble.c

---
+++ src/bubble.c
@@ -0,0 +1,33 @@
+int nums[8];
+
+void bubble(int *data, int num);
+void swap(int *data, int a, int b);
+
+void swap(int *data, int a, int b){
+    int temp;
+    temp = data[a];
+    data[a] = data[b];
+    data[b] = temp;
+}
+
+void bubble(int *data, int num){
+    int i, j;
+    for (i = 0; i < num; i= i+1)
+        for(j= num-1; j>i; j=j-1)
+            if(data[j-1] > data[j])
+                swap(data, j-1, j);
+}
+
+int main(){
+    nums[0] = 5;
+    nums[1] = 2;
+    nums[2] = 7;
+    nums[3] = 10;
+    nums[4] = 1;
+    nums[5] = 3;
+    nums[6] = 4;
+    nums[7] = 8;
+    
+    bubble(nums, 8);
+    return 0;
+}
